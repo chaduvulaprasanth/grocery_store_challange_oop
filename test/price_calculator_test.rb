d70d12b80a4c25fa520d14814548f2c918cf03ab
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-02 13:57:59 +0530

    written tests and calculated saved amount

--- test/price_calculator_test.rb
+++ test/price_calculator_test.rb
@@ -7,4 +7,10 @@ class BillingMachineTest < Minitest::Test
     expected = 19.02
     assert_equal(expected,BillingMachine.new({"milk"=>3, "bread"=>4, "banana"=>1, "apple"=>1}).show_total)
   end
+
+  def test_amount_saved
+    expected = 3.45
+    assert_equal(expected,BillingMachine.new({"milk"=>3, "bread"=>4, "banana"=>1, "apple"=>1}).show_saved_amount)
+  end
+
 end
