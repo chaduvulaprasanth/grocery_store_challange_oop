16438b43fa970f6a06e6c97adf3bdd718f0f55b7
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-02 13:26:58 +0530

    created data in price table

---
+++ lib/price_table.rb
@@ -0,0 +1,10 @@
+module Pricetable
+  MENU =  [
+    {"name" => "milk", "price" => 3.97, "sale_qun" => 2, "sale_price" => 5 },
+    {"name" => "bread", "price" => 2.17, "sale_qun" => 3, "sale_price" => 6 },
+    {"name" => "banana", "price" => 0.99, "sale_qun" => nil, "sale_price" => nil},
+    {"name" => "apple", "price" => 0.89, "sale_qun" => nil, "sale_price" => nil}
+  ]
+  
+  ITEMNAMES = ["milk","bread","banana","apple"]
+end
