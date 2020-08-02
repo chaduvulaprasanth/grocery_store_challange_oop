d5fbc3c5a52316804e152537e5b8f47d2a0e8ff9
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-02 13:50:19 +0530

    written tests and calculated total

--- lib/price_calculator.rb
+++ lib/price_calculator.rb
@@ -1,11 +1,12 @@
 require_relative "price_table"
 
 class OrderEntry
-  attr_reader :item_names, :order
+  attr_reader :item_names, :order, :calculator
 
-  def initialize
+  def initialize(calculator)
     @order = Hash.new(0)
     @item_names = Pricetable::ITEMNAMES
+    @calculator = calculator
   end
 
   def enter_order
@@ -31,7 +32,80 @@ class OrderEntry
 
   def sort_order(items)
     items.select{ |item| @order[item] += 1 if item_names.include?(item)}
+    calculator.checkout(order)
   end
 end
 
-OrderEntry.new.enter_order
+class BillingMachine 
+  attr_reader :total, :menu, :order, :item, :quantity, :price, :sale_qun, :sale_price, :items_price
+
+  def initialize(order)
+    @total = 0
+    @menu = Pricetable::MENU
+    @order = order
+    @item = nil,
+    @quantity = nil,
+    @price = nil,
+    @sale_qun = nil,
+    @sale_price = nil
+    @items_price = nil
+  end
+
+  def self.checkout(order)
+    new(order).checkout
+  end
+
+  def checkout
+    order.each{ |item, quantity| item_in_sale(item, quantity) }
+    puts "$#{total.round(2)}"
+  end
+
+  def item_in_sale(item,quantity)
+    menu.each do |menu_item| 
+      if menu_item["name"] == item
+        @item = item
+        @quantity = quantity
+        @price = menu_item["price"]
+        @sale_qun = menu_item["sale_qun"]
+        @sale_price = menu_item["sale_price"] 
+        billing
+      end
+    end
+  end
+
+  def billing
+    sale_qun && quantity >= sale_qun ? discount_billing : normal_billing
+  end
+
+  def discount_billing
+    price_of_simliar_item_in_sale
+  end
+
+  def normal_billing
+    price_of_simliar_item
+  end
+
+  def price_of_simliar_item_in_sale
+    @items_price = sale_price + (price * (quantity - sale_qun))
+    add_total
+  end
+
+  def price_of_simliar_item
+    @items_price = quantity * price
+    add_total
+  end
+
+  def add_total
+    @total += items_price
+  end
+
+  # for testing total, saved amount and recipt table
+  def show_total
+    checkout
+    total.round(2)
+  end
+
+end
+
+
+OrderEntry.new(BillingMachine).enter_order
