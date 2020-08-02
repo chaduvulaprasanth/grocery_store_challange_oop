34ff1f1f890270b1421869152434eb3025316098
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-02 14:22:00 +0530

    tested & created billing machine receipt

--- test/price_calculator_test.rb
+++ test/price_calculator_test.rb
@@ -3,6 +3,19 @@ require "minitest/pride"
 require_relative "../lib/price_calculator"
 
 class BillingMachineTest < Minitest::Test
+  def test_billing_machine_receipt
+    expected = puts 
+      " +--------+----------+-------+
+      | Item   | Quantity | Price |
+      +--------+----------+-------+
+      | milk   | 3        | 8.97  |
+      | bread  | 4        | 8.17  |
+      | banana | 1        | 0.99  |
+      | apple  | 1        | 0.89  |
+      +--------+----------+-------+ \n Total price : $19.02 \n You saved $3.45 today"
+    assert_equal(expected, BillingMachine.checkout({"milk"=>3, "bread"=>4, "banana"=>1, "apple"=>1}))
+  end
+
   def test_total
     expected = 19.02
     assert_equal(expected,BillingMachine.new({"milk"=>3, "bread"=>4, "banana"=>1, "apple"=>1}).show_total)
